e0e72ecafc9ae422d672cad82ac0aac78e213fdf
popanda <popkova.ann@gmail.com>
2015-11-04 21:57:28 +0100

    First implementation of the new layout / only admin page

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -44,200 +44,106 @@ $globalUSERNAME = $userData["emp_username"];
 </head>
 <body>
 
-<div class="header main_header">
-   <?php echo "Přihlášený uživatel: " . $userData["emp_fullname"] . "($userData[emp_email]), <a class='a_sign_out' href='LogOff.php'>odhlásit</a>"; ?>
-</div>
+        <header>
+            <img src="../Resources/Images/logo.png" alt="LOGO" class="logo" height="120px">
+            <div class="userdata">
+
+               <?php echo "Přihlášený uživatel: " . $userData["emp_fullname"] . "($userData[emp_email])<br> <a class='a_sign_out' href='LogOff.php'>ODHLÁSIT</a>"; ?>
+            </div>
+            <hr class="headerLine">
+        </header>
 
 <?php
 if(!isset($_GET["action"]))
 {
 ?>
-    <div class="boxes">
+     <div class="page">
+         <div class="boxes">
 
         <!-- Part where currently logged employee information is displayed -->
 
-        <div class="ordinary_box_l first_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_s first_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_l first_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_s first_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_l first_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-
         <!-- 1st row */ -->
-        <div class="employee_box ordinary_box_s">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-
 
         <!-- PRIVILEGE LEVEL 1 BOXES -->
 
-        <div class="employee_box">
-            <div class="box_content add_box">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        <a class="a_add" href="Admin.php?action=Add">
-                            <img class="rs" src="../Resources/Images/ikona_plus_mensi.jpg"
-                                 onmouseover="this.src='../Resources/Images/ikona_plus.jpg'"
-                                 onmouseout="this.src='../Resources/Images/ikona_plus_mensi.jpg'"/>
-                        </a>
+            <div class="employee_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            <a href="Admin.php?action=Add">
+                                <img class="rs" src="../Resources/Images/add.png"
+                                     onmouseover="this.src='../Resources/Images/add_selected.png'"
+                                     onmouseout="this.src='../Resources/Images/add.png'"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="employee_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        <a href="Admin.php?action=View">
-                            <img class="rs" src="../Resources/Images/ikona_oko_mensi.jpg"
-                                 onmouseover="this.src='../Resources/Images/ikona_oko.jpg'"
-                                 onmouseout="this.src='../Resources/Images/ikona_oko_mensi.jpg'"/>
-                        </a>
+            <div class="employee_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            <a href="Admin.php?action=View">
+                                <img class="rs" src="../Resources/Images/see.png"
+                                     onmouseover="this.src='../Resources/Images/see_selected.png'"
+                                     onmouseout="this.src='../Resources/Images/see.png'"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="employee_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        <a href="Admin.php?action=Orders">
-                        <img class="rs" src="../Resources/Images/ikona_objednavky_mensi.jpg"
-                             onmouseover="this.src='../Resources/Images/ikona_objednavky.jpg'"
-                             onmouseout="this.src='../Resources/Images/ikona_objednavky_mensi.jpg'"/>
-                        </a>
+            <div class="employee_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            <a href="Admin.php?action=Orders">
+                                <img class="rs" src="../Resources/Images/deal.png"
+                                     onmouseover="this.src='../Resources/Images/deal_selected.png'"
+                                     onmouseout="this.src='../Resources/Images/deal.png'"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="employee_box ordinary_box_s">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
+            <!-- 2nd row */  -->
 
-        <!-- 2nd row */  -->
-        <div class="director_box ordinary_box_s">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
 
-        <div class="director_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        ... CONTENT HERE ...
+            <div class="director_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            ... CONTENT HERE ...
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="director_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        ... CONTENT HERE ...
+            <div class="director_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            ... CONTENT HERE ...
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="director_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell">
-                        ... CONTENT HERE ...
+            <div class="director_box">
+                <div class="box_content">
+                    <div class="box_table">
+                        <div class="box_table-cell">
+                            ... CONTENT HERE ...
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="director_box ordinary_box_s">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
 
-        <div class="ordinary_box_l last_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
         </div>
-        <div class="ordinary_box_s last_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_l last_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_s last_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-        <div class="ordinary_box_l last_line_box">
-            <div class="box_content">
-                <div class="box_table">
-                    <div class="box_table-cell"></div>
-                </div>
-            </div>
-        </div>
-
-    </div>
+     </div>
     <?php
 }
 else
--- Admin/index.php
+++ Admin/index.php
@@ -18,19 +18,44 @@ if(isset($_SESSION["emp_id"]) && isset($_SESSION["emp_hash"]))
     }
 }
 ?>
-<form method="post" action="LogOn.php">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/BackendStyles.css">
+    <title></title>
+</head>
+
+<body>
+<div class="page">
+
+<header>
+
+    <img src="../Resources/Images/logo.png" alt="LOGO" class="logo" height="120px">
+    <div class="headline">Přihlášení do systému</div>
+    <hr class="headerLine">
+
+</header>
+<form class="signInForm" method="post" action="LogOn.php">
     <table>
         <tr>
             <td>Přihlašovací jméno:</td>
-            <td><input type="text" name="logon_username"></td>
+            <td class="thin"></td>
+            <td><input type="text" name="logon_username" class="text"></td>
         </tr>
         <tr>
             <td>Heslo:</td>
-            <td><input type="password" name="logon_password"></td>
+            <td class="thin"></td>
+            <td><input type="password" name="logon_password" class="text"></td>
         </tr>
+
         <tr>
             <td></td>
-            <td><input type="submit" value="Přihlásit se do systému"></td>
+            <td class="thin"><input type="submit" value="PŘIHLÁSIT" class="button"></td>
+            <td></td>
         </tr>
     </table>
 </form>
+</div>
+</body>
